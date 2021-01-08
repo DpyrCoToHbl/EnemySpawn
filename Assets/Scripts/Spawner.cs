3e4944bd9feb6c41da974281cb199f1820acb8ea
DpyrCoToHbl <DpyrCoToHbl@users.noreply.github.com>
2021-01-09 00:53:06 +0300

    init

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject[] zombies = new GameObject[3];
+    [SerializeField] GameObject[] spawnPoints = new GameObject[23];
+    [SerializeField] AudioSource _zombieSound;
+    private List<Vector3> _usedSpawnPoints = new List<Vector3>();
 
-    private int zombieSpawnAmount = 12;
+    private int zombieSpawnAmount = 10;
     private int _secondsBetweenSpawn = 2;
 
     private void Start()
     {
-        StartCoroutine(spawnZombie());
+        StartCoroutine(SpawnZombie());
     }
 
-    private IEnumerator spawnZombie()
+    private IEnumerator SpawnZombie()
     {
         int zombieCount = 0;
+
         while (zombieCount < zombieSpawnAmount)
         {
-            int spawnPositionX = Random.Range(-8, 2);
-            int spawnPositionY = Random.Range(-4, 4);
-
             var zombie = zombies[Random.Range(0, zombies.Length)];
+            var spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
 
-            GameObject newObject = Instantiate(zombie, new Vector3(spawnPositionX, spawnPositionY, 0), Quaternion.identity);
+            if (!_usedSpawnPoints.Contains(spawnPoint.transform.position))
+            {
+                GameObject newZombie = Instantiate(zombie, spawnPoint.transform.position, Quaternion.identity);
+                _zombieSound.Play();
+                zombieCount++;
+            }
 
-            zombieCount++;
+            _usedSpawnPoints.Add(spawnPoint.transform.position);
 
             yield return new WaitForSeconds(_secondsBetweenSpawn);
         }
